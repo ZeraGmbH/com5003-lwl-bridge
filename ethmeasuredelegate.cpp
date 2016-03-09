be699bf39f71def44a5ce639daeae8f59aca074c
Peter Lohmer <peterlohmer01@gmail.com>
2016-03-09 10:10:52 +0100

    ethmeasuredelegate.cpp: bugfix deploying dft actual values
    Signed-off-by: Peter Lohmer <peterlohmer01@gmail.com>

--- ethmeasuredelegate.cpp
+++ ethmeasuredelegate.cpp
@@ -209,7 +209,7 @@ void cETHMeasureDelegate::receiveAnswer()
                     // we have found a key that was expected for a dft value so we have to compute the angle
                     QStringList sl;
                     sl = data.split(',');
-                    *(m_ActualValuesDecodeHash[m_ActualDFTDecodeHash[key] ]) = userAtan(sl.at(1).toDouble(&ok), sl.at(0).toDouble(&ok));
+                    *(m_ActualValuesHash[m_ActualDFTDecodeHash[key] ]) = userAtan(sl.at(1).toDouble(&ok), sl.at(0).toDouble(&ok));
 
                 }
 
